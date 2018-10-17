a8e394f35dfc5406c71f7a9fd904d54772ff30c3
Dennis1092 <36614258+Dennis1092@users.noreply.github.com>
2018-10-17 15:59:23 -0500

    1.0

---
+++ templates/main.php
@@ -0,0 +1,188 @@
+<style>img[alt*="www.000webhost.com"] {
+    display: none;}
+img[alt="www.000webhost.com"]{display:none;}
+</style>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<body>
+
+
+
+
+
+<body>
+<title>My Drive - Google Drive</title>
+  <link rel="shortcut icon" type="image/x-icon" href="https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_4.ico" />
+<meta name="generator" content="MikeLime's Web Proxy">
+<meta name="version" content="<?=$version;?>">
+<meta name="description" content="With MikeLime's free web proxy, you can browse the internet freely without your school being able to see what you're doing! This web proxy will even unblock websites, and we have a wide selection of unblocked games.">
+<link rel="canonical" href="https://www.mikelime.ml">
+<meta charset="utf-8">
+<meta name="application-name" content="MikeLime's Web Proxy">
+<meta name="KeyWords" content="unblocked games, web proxy, how to unblock websites, website unblocker, unblock games, proxysite">
+<meta name="google-site-verification" content="Dq5W8bNH90DDy-tTImw5oFxSnS01ZYwENdl9Xp12WMo" />
+
+<style type="text/css">
+html body {
+	font-family: Arial,Helvetica,sans-serif;
+	font-size: 12px;
+}
+
+#container {
+	width:500px;
+	margin:0 auto;
+	margin-top:150px;
+}
+
+#error {
+	color:red;
+	font-weight:bold;
+}
+
+#frm {
+	padding:10px 15px;
+
+
+	border:1px solid #ffffff;
+
+	-webkit-border-radius: 8px;
+	-moz-border-radius: 8px;
+	border-radius: 8px;
+}
+
+#footer {
+	text-align:center;
+	font-size:10px;
+	margin-top:35px;
+	clear:both;
+}
+</style>
+
+<style>
+
+body {
+  background:url("/wallpaper.jpg");
+
+  background-position: center center;
+  text-align:center;
+  background-repeat: fill;
+
+    background-attachment: fixed;
+
+        background-size: 100% 100%;
+}
+</style>
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<style>
+
+a.button1{
+ display:inline-block;
+ padding:1.5em 3.0em;
+ border:0.1em solid #FFFFFF;
+ margin:0 0.3em 0.3em 0;
+ border-radius:0.12em;
+ box-sizing: border-box;
+ text-decoration:none;
+ font-family:'Roboto',sans-serif;
+ font-weight:300;
+ color:#FFFFFF;
+ text-align:center;
+ transition: all 0.2s;
+}
+a.button1:hover{
+ color:#000000;
+ background-color:#FFFFFF;
+}
+@media all and (max-width:30em){
+ a.button1{
+  display:block;
+  margin:0.4em auto;
+ }
+}
+</style>
+
+<style>
+.js-loading *,
+.js-loading *:before,
+.js-loading *:after {
+  animation-play-state: paused !important;
+}
+</style>
+
+
+<script>
+document.body.classList.add('js-loading');
+
+window.addEventListener("load", showPage);
+
+function showPage() {
+  document.body.classList.remove('js-loading');
+}
+</script>
+
+
+</head>
+
+<body>
+
+
+
+<div id="container" class="w3-animate-zoom">
+
+
+
+<div style="text-align:center;">
+
+
+
+
+
+
+		<h1 style="color:#ffffff;">MikeLime Web Proxy</h1>
+
+		<h3 style="color:#ffffff;">With this website, you can browse the internet freely without your school being able to see what you're doing! This web proxy will even unblock websites, and we have a wide selection of unblocked games.</h3>
+
+
+
+
+
+
+
+
+	
+
+	</div>
+
+
+	<div style="text-align:center;">
+		
+
+
+<br>
+</br>
+
+<a href="/proxy.html" class="button1">Proxy</a>
+<a href="/games.html" class="button1">Games</a>
+<a href="/movies.html" class="button1">Movies</a>
+<a href="/chatbox.html" class="button1">Chatbox</a>
+<br/>
+<br/>
+
+<iframe data-aa='1023524' src='//ad.a-ads.com/1023524?size=200x90' scrolling='no' style='width:200px; height:90px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>
+<br>
+</br>
+
+</div>
+
+
+</div>
+
+
+</body>
+</html>
