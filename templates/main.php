0e19ba7459985d26b66dbc08e59bcfe8c10728be
Dennis1092 <36614258+Dennis1092@users.noreply.github.com>
2018-10-18 17:09:01 -0500

    hi

--- templates/main.php
+++ templates/main.php
@@ -1,63 +1,158 @@
-<!DOCTYPE HTML>
+<style>img[alt*="www.000webhost.com"] {
+    display: none;}
+img[alt="www.000webhost.com"]{display:none;}
+</style>
 <html>
 
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Google</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="shortcut icon" type="image/x-icon" href="https://www.google.com/favicon.ico">
+<body>
+<title>MikeLime Proxy</title>
+
+<meta name="generator" content="php-proxy.com">
+<meta name="version" content="<?=$version;?>">
+
+<style type="text/css">
+html body {
+	font-family: Arial,Helvetica,sans-serif;
+	font-size: 12px;
+}
+
+#container {
+	width:500px;
+	margin:0 auto;
+	margin-top:50px;
+}
+
+#error {
+	color:red;
+	font-weight:bold;
+}
+
+#frm {
+	padding:10px 15px;
+
+
+	border:1px solid #ffffff;
+
+	-webkit-border-radius: 8px;
+	-moz-border-radius: 8px;
+	border-radius: 8px;
+}
+
+#footer {
+	text-align:center;
+	font-size:10px;
+	margin-top:35px;
+	clear:both;
+}
+</style>
+
+<style>
+
+a.button1{
+ display:inline-block;
+ padding:1.5em 3.0em;
+ border:0.1em solid #FFFFFF;
+ margin:0 0.3em 0.3em 0;
+ border-radius:0.12em;
+ box-sizing: border-box;
+ text-decoration:none;
+ font-family:'Roboto',sans-serif;
+ font-weight:300;
+ color:#FFFFFF;
+ text-align:center;
+ transition: all 0.2s;
+}
+a.button1:hover{
+ color:#000000;
+ background-color:#FFFFFF;
+}
+@media all and (max-width:30em){
+ a.button1{
+  display:block;
+  margin:0.4em auto;
+ }
+}
+</style>
+
+<style>
+.js-loading *,
+.js-loading *:before,
+.js-loading *:after {
+  animation-play-state: paused !important;
+}
+</style>
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<script>
+document.body.classList.add('js-loading');
+
+window.addEventListener("load", showPage);
+
+function showPage() {
+  document.body.classList.remove('js-loading');
+}
+</script>
+
+
+
 </head>
 
 <body>
-    <div class="container">
-        <div class="banner">
-            <span id="mail"><a href="#">Gmail</a></span>
-            <span id="images"><a href="#">Images</a></span>
-            <span id="apps"><!-- App Icon --></span>
-            <span id="login">
-				<button id="signin">Sign In</button>
-			</span>
-        </div>
-        <div class="logo">
-            <span id="google">
-				<img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" />
-			</span>
-        </div>
-	<form action="index.php" id="form" method="post">
-        <div class="search">
-            <input name="url" type="text" autocomplete="off"/>
-        </div>
-        <div class="buttons">
-            <button id="google-search">Google Search</button>
-            <button id="lucky">I'm Feeling Lucky</button>
-        </div>
-	    </form>
-	<?php if(isset($error_msg)){ ?>
-	
-	<div id="error">
-	<div class="message" style="text-align: center;"><?php echo $error_msg; ?></div>
-	</div>
-	
-	<?php } ?>
-    </div>
-    <div class="footer">
-        <span class="left">
-			<span id="ads"><a href="#">Advertising</a></span>
-	        <span id="business"><a href="#">Business</a></span>
-	        <span id="about"><a href="#">About</a></span>
-        </span>
-        <span class="right">
-        	<span id="ads"><a href="#">Privacy</a></span>
-	        <span id="business"><a href="#">Terms</a></span>
-	        <span id="about"><a href="#">Settings</a></span>
-        </span>
-    </div>
-  	  <script>
-	   	var form = document.getElementById("form");
-		document.getElementById("google-search").addEventListener("click", function () {
-		  form.submit();
-		});
-	</script>
-</body>
 
+
+
+
+
+<div id="container" class="w3-animate-zoom">
+
+
+<a href="/index.html" class="button1">Go Home</a>
+
+	<div style="text-align:center;">
+		<h1 style="color:#ffffff;">MikeLime's Web Proxy</h1>
+		<h3 style="color:#ffffff;">With MikeLime's free web proxy, you can browse the internet freely without your school being able to see what you're doing! This web proxy will even unblock websites, and we have a wide selection of unblocked games.</h3>
+	</div>
+<br/>
+<br/>
+	<?php if(isset($error_msg)){ ?>
+
+	<div id="error">
+		<p><?php echo $error_msg; ?></p>
+	</div>
+
+	<?php } ?>
+
+	<div id="frm">
+
+	<!-- I wouldn't touch this part -->
+
+		<form action="index.php" method="post" style="margin-bottom:0;">
+			<input name="url" type="text" style="width:400px;" autocomplete="off" placeholder="http://" />
+			<input type="submit" value="Go" />
+		</form>
+
+		<script type="text/javascript">
+			document.getElementsByName("url")[0].focus();
+		</script>
+
+	<!-- [END] -->
+
+	</div>
+<br/>
+<br/>
+</div>
+	<div style="text-align:center;" class="w3-animate-zoom">
+		<h3 style="color:#ffffff;">Just type a website in the bar above and click go!</h2>
+      <br/>
+      <br/>
+      <iframe data-aa='1023524' src='//ad.a-ads.com/1023524?size=200x90' scrolling='no' style='width:200px; height:90px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>
+</div>
+
+
+
+</body>
 </html>
