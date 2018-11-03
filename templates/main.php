da93fa1b55c955d5b81f592049a1c018414838eb
Dennis1092 <36614258+Dennis1092@users.noreply.github.com>
2018-11-03 09:19:27 -0500

    bet

--- templates/main.php
+++ templates/main.php
@@ -7,6 +7,17 @@ img[alt="www.000webhost.com"]{display:none;}
 <!DOCTYPE html>
 <html>
 <head>
+
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-KCF9WRC');</script>
+  <!-- End Google Tag Manager -->
+
+
+
 <body>
 <title>My Drive - Google Drive</title>
     <link rel="icon" href="https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_4.ico">
@@ -115,7 +126,10 @@ function showPage() {
 
 <body>
 
-
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCF9WRC"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 
 
 
